9754a909548cb87273ef57b2a1f6c91ab2f5e1b5
Hellsive <49934100+Hellsive@users.noreply.github.com>
2019-11-04 08:27:47 -0500

    Add files via upload
    Quick File for renaming Geometry, joints, and cameras if needed.

---
+++ Renaming_Script.py
@@ -0,0 +1,45 @@
+from maya import cmds
+
+selection = cmds.ls(selection=True, long=True)
+
+print selection
+
+# Checks selected object
+if len(selection) == 0:
+    selection = cmds.ls(long=True, dag=True)
+
+selection.sort(key=len, reverse=True)
+
+for obj in selection:
+
+    shortName = obj.split('|')[-1]
+
+    print "Original: ", shortName
+
+    children = cmds.listRelatives(obj, children=True) or []
+
+# Checks if there are children to selected object.
+    if len(children) == 1:
+        child = children[0]
+        objType = cmds.objectType(child)
+    else:
+        objType = cmds.objectType(obj)
+
+# Object type and renaming suffix function
+    if objType == "mesh":
+        suffix = 'GEO'
+    elif objType == "joint":
+        suffix = 'JNT'
+    elif objType == 'camera':
+        print "No Camera Needed"
+        continue
+    else:
+        suffix = 'grp'
+
+    # Creates new shortened suffix
+    newName = shortName+"_"+suffix
+
+    # Adds new title to selected objects
+    cmds.rename(obj, newName)
+
+    print "New Name: ", newName
